d73dc9b69a22afa1eb88a3a652a9ec6455a3bd6e
Matheus Obando da Silva <mos.snf18@uea.edu.br>
2021-05-25 00:39:08 -0300

    Prolog assignment, adding base chatbot

---
+++ Prolog/chatbot.pl
@@ -0,0 +1,35 @@
+content_database('cinema', ['Conteudo sobre cinema 1', 'Conteudo sobre cinema 2']).
+
+content_database('esportes', ['Conteudo sobre carros 1', 'Conteudo sobre esportes 2']).
+
+content_database('ciencias', ['Conteudo sobre ciencias 1', 'Conteudo sobre ciencias 2']).
+
+content_database('games', ['Conteudo sobre games 1', 'Conteudo sobre games 2']).
+
+content_database('filosofia', ['Conteudo sobre filosofia 1', 'Conteudo sobre filosofia 2']).
+
+content_database('curiosidades', ['Conteudo sobre curiosidades 1', 'Conteudo sobre curiosidades 2']).
+
+content_database('fim', ['Foi um prazer ajudar, tenha um bom dia 1', 'Foi um prazer ajudar, tenha um bom dia 2']).
+
+
+get_random_content(List, Content) :- 
+    length(List, Length),
+    random(0, Length, Index), 
+    nth0(Index, List, Content).
+
+search_database(Input, Output) :- content_database(Input, List), get_random_content(List, Output).
+
+end_session(Input) :- Input = ('fim').
+
+chatbot :-
+
+    format('Ola! Eu sou um chatbot de pesquisa por conteudos'), nl,
+    repeat,
+    format('Sobre o que você gostaria de pesquisar?'), nl,
+
+    read(Input), nl,
+
+    search_database(Input, Output),
+    format(Output), nl, nl,
+    end_session(Input).
