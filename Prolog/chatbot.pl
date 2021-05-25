bcd1b2b471a6facebae96ff84b0680b726670400
Matheus Obando da Silva <mos.snf18@uea.edu.br>
2021-05-25 05:31:23 -0300

    Prolog Assignment, adding pattern matching base

--- Prolog/chatbot.pl
+++ Prolog/chatbot.pl
@@ -1,12 +1,12 @@
 content_database('cinema', ['Conteudo sobre cinema 1', 'Conteudo sobre cinema 2']).
 
-content_database('esportes', ['Conteudo sobre carros 1', 'Conteudo sobre esportes 2']).
+content_database('esportes', ['Conteudo sobre carros 1','Conteudo sobre esportes 2']).
 
 content_database('ciencias', ['Conteudo sobre ciencias 1', 'Conteudo sobre ciencias 2']).
 
 content_database('games', ['Conteudo sobre games 1', 'Conteudo sobre games 2']).
 
-content_database('filosofia', ['Conteudo sobre filosofia 1', 'Conteudo sobre filosofia 2']).
+content_database('filosofia',['Conteudo sobre filosofia 1','Conteudo sobre filosofia 2']).
 
 content_database('curiosidades', ['Conteudo sobre curiosidades 1', 'Conteudo sobre curiosidades 2']).
 
@@ -18,7 +18,15 @@ get_random_content(List, Content) :-
     random(0, Length, Index), 
     nth0(Index, List, Content).
 
-search_database(Input, Output) :- content_database(Input, List), get_random_content(List, Output).
+find_pattern([], _).
+find_pattern([X|Y], List) :-
+
+    (content_database(X, List) -> !; find_pattern(Y, List)).
+
+search_database(Input, Output) :-
+    atomic_list_concat(AtomList, ' ', Input),
+    find_pattern(AtomList, List),
+    get_random_content(List, Output).
 
 end_session(Input) :- Input = ('fim').
 
@@ -29,7 +37,6 @@ chatbot :-
     format('Sobre o que você gostaria de pesquisar?'), nl,
 
     read(Input), nl,
-
     search_database(Input, Output),
     format(Output), nl, nl,
     end_session(Input).
