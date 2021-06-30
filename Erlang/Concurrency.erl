c28d00deac35f90b3eb66b7a3dd0ae2109e9ecac
Matheus Obando da Silva <mos.snf18@uea.edu.br>
2021-06-30 08:46:49 -0300

    Adding Erlang Concurrency code

---
+++ Erlang/Concurrency.erl
@@ -0,0 +1,21 @@
+cd -module(echo).
+-export([go/0, loop/0]).
+
+
+go() ->
+    Pid2 = spawn(echo, loop, []),
+    Pid2 ! {self(), hello},
+    receive
+        {Pid2, Msg} ->
+        io:format("P1 ~w~n",[Msg])
+    end,
+    Pid2 ! stop.
+
+
+loop() ->
+receive
+    {From, Msg} ->
+        From ! {self(), Msg},
+        loop();
+    stop ->
+end.
